ccb27c29ea71a80b871d7ccc74cb30634182a856
mikdevio <villacis.mhva@gmail.com>
2026-05-14 12:06:14 -0500

    refactor: Complete codewars kata digital root

--- codewars_katas/008_digital_root/solution.ts
+++ codewars_katas/008_digital_root/solution.ts
@@ -6,6 +6,42 @@
   will be a non-negative integer. */
 
 export const digitalRoot = (n: number): number => {
-  const arrayStr = n.toSting().split("")
-  return arrayStr.map(Number).reduce((a, value) => a+value, 0);
+  const arrayStr = n.toString().split("")
+  const sumNumber = arrayStr.map(Number).reduce((a, value) => a+value, 0);
+  if(sumNumber === arrayStr.map(Number)[0]){
+    return sumNumber;
+  }
+  return digitalRoot(sumNumber);
 };
+
+describe("solution", () => {
+  it('should work for fixed tests', () => {
+    assert.equal(digitalRoot(16), 7);
+    assert.equal(digitalRoot(195), 6);
+    assert.equal(digitalRoot(992), 2);
+    assert.equal(digitalRoot(999999999999), 9);
+    assert.equal(digitalRoot(167346), 9);
+    assert.equal(digitalRoot(10), 1);
+    assert.equal(digitalRoot(0), 0);
+    assert.equal(digitalRoot(200), 2);
+  });
+  
+  it('should work for random tests', () => {
+    
+    const myDigitalRoot = (n:number):number => {
+      return n < 10 ?
+             n :
+             myDigitalRoot([...n.toString()].map(Number).reduce((acc, cur) => acc + cur, 0));
+    };
+
+    const generateRandom = (): number[] => {
+      const biggestNumber: number = 999999999999;
+      let arrOfRandom: number[] = Array.from({length: 10}, (_) => Math.random());
+      return arrOfRandom.map(n => Math.floor(n * biggestNumber));
+    };
+    
+    generateRandom().forEach(n => {
+      assert.equal(digitalRoot(n), myDigitalRoot(n), `Testing for n = ${n}`);
+    });
+  });
+});
